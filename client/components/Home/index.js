7fca9828310c7dffb8fc8d1d38f212b0a5eafc42
William Cox <williamcox@Williams-MBP.lan>
2018-02-13 22:26:06 -0700

    minor fixes

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -8,11 +8,6 @@ class Home extends Component {
     super();
   }
 
-  componentDidMount() {
-    var thumbs = document.getElementsByClassName("Thumbnail");
-    reveal(thumbs, 0);
-  }
-
   render() {
 
     let posts = createPostThumbnails(this.props.posts.posts);
@@ -34,16 +29,6 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Home);
-
-function reveal(thumbs, index) {
-  setTimeout(function() {
-    thumbs[index].style.opacity = 1.0;
-    index++;
-    if (thumbs[index]) {
-      reveal(thumbs, index);
-    }
-  }, 100);
-}
 
 function createPostThumbnails(posts) {
   return posts.map((post) => {
--- client/components/Thumbnail/index.js
+++ client/components/Thumbnail/index.js
@@ -5,6 +5,11 @@ class Thumbnail extends Component {
 
   constructor() {
     super();
+    this.reveal = this.reveal.bind(this);
+  }
+
+  reveal(e) {
+    e.target.parentElement.parentElement.style.opacity = 1;
   }
 
   render() {
@@ -12,7 +17,7 @@ class Thumbnail extends Component {
     return (
       <div className="Thumbnail">
         <a className="thumb-link flex jc-c ai-c fd-c" href="/#/post">
-          <img src={this.props.post.thumbnail} />
+          <img src={this.props.post.thumbnail} onLoad={this.reveal} />
           <h1>{this.props.post.work}</h1>
           <h1>{this.props.post.title}</h1>
         </a>
