fedb4165d24731f8da132884fc07e763b6170a08
William Cox <williamcox@Williams-MBP.lan>
2018-02-19 21:28:03 -0700

    updated mongo stuff

--- server_modules/get-posts.js
+++ server_modules/get-posts.js
@@ -1,11 +1,11 @@
-const env = require('dotenv').config();
 const config = require('../config');
 const mongoClient = require('mongodb').MongoClient;
-const mongoURI =  (config.mongoURI || env.MONGO);
+const uri = require('./get-mongo-uri')();
+const mongoURI =  (config.mongoURI || uri);
 
 module.exports = () => {
   return new Promise(function(resolve, reject) {
-    mongoClient.connect(mongoURI, (err, db) => {
+    mongoClient.connect(uri, (err, db) => {
       var collection = db.collection('posts');
       collection.find({}, (err, result) => {
         result = result.sort({id: 1});
--- server_modules/index.js
+++ server_modules/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  getPosts: require('./get-posts')
+  getPosts: require('./get-posts'),
+  getMongoUri: require('./get-mongo-uri')
 }
