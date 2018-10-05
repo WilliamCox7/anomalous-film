1cfea52ffae5496ac9302dbd3e0838a3d5b6faff
William Cox <willubyu7@gmail.com>
2018-10-05 02:43:37 -0600

    no ssl

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const https = require('https');
-const fs = require('fs');
-const config = require('./config');
 const app = module.exports = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -16,12 +13,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
-const options = {
-  key: fs.readFileSync(config.key),
-  cert: fs.readFileSync(config.cert)
-};
-
-https.createServer(options, app)
-.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
   console.log('localhost:' + app.get('port'));
 });
