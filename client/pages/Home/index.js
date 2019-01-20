1499695558fe48fe83be9a1872186a4a66a5c22a
William Cox <willubyu7@gmail.com>
2019-01-19 20:54:03 -0700

    updates to init

--- client/pages/Home/index.js
+++ client/pages/Home/index.js
@@ -9,17 +9,20 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      poster: "https://i.pinimg.com/originals/13/38/7b/13387b189da77d448e2e8b73bef3e9bb.jpg",
-      posterPos: "2px -96px",
-      like: true,
-      award: true,
-      title: "Whiplash",
-      released: new Date(2011, 3, 11),
-      tagline: "You can erase someone from your mind. Getting them out of your heart is another story.",
-      rating: 8.3,
-      imdb: 6.9,
-      viewed: new Date(2019, 0, 15),
-      location: "Megaplex at Thanksgiving Point"
+      list: [{name: 'test'}],
+      highlighted: {
+        poster: "https://i.pinimg.com/originals/13/38/7b/13387b189da77d448e2e8b73bef3e9bb.jpg",
+        posterPos: "2px -96px",
+        like: true,
+        award: true,
+        title: "Whiplash",
+        released: new Date(2011, 3, 11),
+        tagline: "You can erase someone from your mind. Getting them out of your heart is another story.",
+        rating: 8.3,
+        imdb: 6.9,
+        viewed: new Date(2019, 0, 15),
+        location: "Megaplex at Thanksgiving Point"
+      }
     }
   }
 
@@ -28,10 +31,18 @@ class Home extends Component {
   }
 
   render() {
+
+    let list = this.state.list.map((item, i) => {
+      return <div key={i}>{item.name}</div>
+    });
+
     return (
       <div id="Home">
-        <Filter />
-        <Film film={this.state} />
+        <div className="film-wrapper">
+          <Filter />
+          <Film film={this.state.highlighted} />
+        </div>
+        {list}
       </div>
     );
   }
