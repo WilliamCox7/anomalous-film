41a065a9ae55aa59a260254b5d58b31139f15c25
William Cox <willubyu7@gmail.com>
2019-01-27 23:20:26 -0700

    quick fix

--- client/pages/Home/index.js
+++ client/pages/Home/index.js
@@ -11,7 +11,7 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      list: [],
+      list: [{}],
       filter: {
         column: 'viewed',
         ascending: true,
